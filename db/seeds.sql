543dc8d25a1bbe5c9e1dd15090bec38fa4a16a58
unknown <amkadilak@gmail.com>
2018-01-04 02:41:43 -0500

    finally got it to accept an inputgit add -A

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+-- Seed Burgers into Database
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Nick's Cheeseburger with Grilled Onions", false, "2017-12-31 23:30:00"),
+  ("The Burger", false, "2017-12-31 23:35:01"),
+  ("Cheeseburg, Fried Onions", false, "2017-12-31 23:40:02"),
+  ("The Tavern Burger", false, "2017-12-31 23:45:03"),
+  ("Proper Burger", false, "2017-12-31 23:50:04"),
+  ("The Ozersky Burger", false, "2017-12-31 23:55:05"),
+  ("The Mott Burger", false, "2018-01-01 00:00:00"),
+  ("Burger with Cheese All the Way", true, "2018-01-01 01:00:01"),
+  ("Classic Burger with Sharp American", true, "2018-01-01 02:00:02"),
+  ("Drive-In Cheeseburger with Grilled Onions", true, "2018-01-01 03:00:03");
